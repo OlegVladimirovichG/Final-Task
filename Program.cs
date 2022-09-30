49a42c5cb650621d1920cce329a22146acfe93d6
OlegVladimirovichG <hankat@mail.ru>
2022-09-30 20:40:34 +0300

    output

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿string[] array1 = new string[5] {"Hello", "2", "world", ":-)" };
+﻿string[] array1 = new string[4] {"Hello", "2", "world", ":-)" };
 string[] array2 = new string[array1.Length];
 
 void SecondArrayWithIF(string[] array1, string[] array2)
